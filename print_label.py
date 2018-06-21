9ff108df05d1a560b2f88eb329b0f62cd9f5c0ef
Pål Ellingsen <pale@unis.no>
2018-06-21 10:51:35 +0200

    Moved the scripts and src folder

---
+++ print_label.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+'''
+ -- Creates and prints labels for AeN
+
+ This program enables the creation and printing of labels 
+
+
+
+@author:     Pål Ell8ingsen
+
+@copyright:  2018 UNIS. 
+
+
+@contact:    pale@unis.no
+@deffield    updated: Updated
+'''
+
+import sys
+import os
+import time
+import uuid
+import warnings
+import datetime as dt
+
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.bubble import Bubble
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.properties import BooleanProperty
+from kivy.uix.label import Label
+from kivy.properties import NumericProperty
+
+from kivy.config import Config
+
+__all__ = []
+__version__ = 0.1
+__date__ = '2018-05-25'
+__updated__ = '2018-06-04'
+
+DEBUG = 1
+TESTRUN = 0
+PROFILE = 0
+
+
+def new_hex_uuid():
+    """
+    Generate a new hex UUID based on the host MAC address and time
+
+    Returns
+    ----------
+    uuid : string
+           uuid expressed as a hex value
+    """
+    return uuid.uuid1().hex  # Based on host ID and time
+
+
+def create_label(uuid, text1, text2, text3, text4):
+    """
+    Creates the ZPL code for the label.
+    Adds a text with the 8 first chracters from the uuid for ease of reading
+
+    Parameters
+    ----------
+    uuid : str
+        The 32 characters hex uuid
+
+    text1 : str
+        First line of text, limited to 18 characters
+
+    text2 : str
+        Second line of text, limited to 18 characters
+
+    text3 : str
+        Third line of text, limited to 18 characters
+
+    text4 : str
+        Fourth line of text, limited to 18 characters    
+
+    Returns
+    ----------
+    zpl : str
+        The formatted ZPL code string that should be sent to the Zebra printer 
+    """
+
+    # This uses a template made with ZebraDesigner, replacing the variables
+    # with the necessary text {X}.
+    zpl = '''
+    CT~~CD,~CC^~CT~
+    ^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4~SD15^JUS^LRN^CI0^XZ
+    ^XA
+    ^MMT
+    ^PW1228
+    ^LL0295
+    ^LS0
+    ^BY110,110^FT51,125^BXN,5,200,22,22,1,~
+    ^FH\^FD{0}^FS
+    ^FT8,267^A0N,21,21^FH\^FD{4}^FS
+    ^FT8,231^A0N,21,21^FH\^FD{3}^FS
+    ^FT9,198^A0N,21,21^FH\^FD{2}^FS
+    ^FT9,166^A0N,21,21^FH\^FD{1}^FS
+    ^FT8,27^A0R,21,21^FH\^FD{5}^FS
+    ^PQ1,0,1,Y^XZ'''.format(uuid, text1, text2, text3, text4, uuid[:8])
+
+    print(zpl)
+    return zpl
+
+
+class LimitText(TextInput):
+    """
+    Overriding TextInput to enable a limited length text field
+
+    Properties
+    ----------
+
+    max_characters : int
+        The max number of characters
+
+    inc_num : Boolean
+        Should the number be increased after a run
+    """
+
+    max_characters = NumericProperty(0)
+    inc_num = BooleanProperty(0)
+
+
+class LabelWidget(FloatLayout):
+
+    def activate_checkbox(self, checkbox, id):
+        if id == 'date':
+
+            self.ids.text1.text = checkbox.active * dt.date.today().isoformat()
+        elif id == 'test':
+
+            self.ids.text2.text = checkbox.active * 'This is a test'
+        elif id == 'inc_num':
+
+            self.ids.text4.inc_num = True
+
+
+class LabelApp(App):
+
+    def build(self):
+        widget = LabelWidget()
+        self.widget = widget
+        return widget
+
+    def print_label(self, *args):
+        print("Printing label")
+        text1 = self.widget.ids.text1.text
+        text2 = self.widget.ids.text2.text
+        text3 = self.widget.ids.text3.text
+        text4 = self.widget.ids.text4.text
+        for n in range(int(self.widget.ids.number.text)):
+
+            zpl = create_label(new_hex_uuid(), text1, text2, text3, text4)
+            # Increase number on prints
+            if self.widget.ids.text4.inc_num:
+                try:
+                    text4 = str(int(text4) + 1)
+                    self.widget.ids.text4.text = text4
+                except ValueError:
+                    warnings.warn(
+                        "WARNING: Text line 4 is not a number, can't increment", UserWarning)
+
+                    time.sleep(seconds / 1e6)  # Wait 1 us
+
+
+def main(argv=None):  # IGNORE:C0111
+    '''Command line options.'''
+
+    try:
+        args = parse_options()
+        Config.set('graphics', 'width', '400')
+        Config.set('graphics', 'height', '400')
+        Config.write()
+        LabelApp().run()
+        return 0
+    except KeyboardInterrupt:
+        ### handle keyboard interrupt ###
+        return 0
+
+
+def parse_options():
+    """
+    Parse the command line options and return these. Also performs some basic
+    sanity checks, like checking number of arguments.
+    """
+    program_version = "v%s" % __version__
+    program_build_date = str(__updated__)
+    program_version_message = '%%(prog)s %s (%s)' % (
+        program_version, program_build_date)
+    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    program_license = '''%s
+
+  Created by Pål Ellingsen on %s.
+
+  Distributed on an "AS IS" basis without warranties
+  or conditions of any kind, either express or implied.
+
+    USAGE
+    ''' % (program_shortdesc, str(__date__))
+
+    # Setup argument parser
+    parser = ArgumentParser(description=program_license,
+                            formatter_class=RawDescriptionHelpFormatter)
+#     parser.add_argument('output', help='''The output file''')
+    parser.add_argument("-v", "--verbose", dest="verbose", action="count", default=0,
+                        help="set verbosity level [default: %(default)s]")
+    parser.add_argument('-V', '--version', action='version',
+                        version=program_version_message)
+
+    # Process arguments
+    args = parser.parse_args()
+
+    if args.verbose > 0:
+        print("Verbose mode on")
+
+    return args
+
+if __name__ == "__main__":
+    if DEBUG:
+        sys.argv.append("-v")
+    sys.exit(main())
