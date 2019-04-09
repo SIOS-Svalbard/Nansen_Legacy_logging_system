d1f2d9f635ef643e93eea3d617938507f729236f
Pål Ellingsen <pale@unis.no>
2019-04-09 16:16:26 +0200

    Added GPL 3

--- src/print_circular_label.py
+++ src/print_circular_label.py
@@ -6,9 +6,21 @@
  This program enables the creation and printing of labels of the 10 mm 
  circular type
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-@author:     Pål Ell8ingsen
+@author:     Pål Ellingsen
 
 @copyright:  2018 UNIS. 
 
@@ -114,8 +126,18 @@ def parse_options():
 
   Created by Pål Ellingsen on %s.
 
-  Distributed on an "AS IS" basis without warranties
-  or conditions of any kind, either express or implied.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
     USAGE
     ''' % (program_shortdesc, str(__date__))
