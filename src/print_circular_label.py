4543043c6b6a35c9ceb4458008e6b9f7f4d6ae69
Pål Ellingsen <pale@unis.no>
2019-05-06 15:29:11 +0200

    Changed to Brady labels

--- src/print_circular_label.py
+++ src/print_circular_label.py
@@ -43,41 +43,61 @@ from argparse import RawDescriptionHelpFormatter
 __all__ = []
 __version__ = 0.1
 __date__ = '2018-07-03'
-__updated__ = '2018-07-05'
+__updated__ = '2019-05-06'
 
 DEBUG = 1
 
 
 def create_label():
     """
-    Creates the ZPL code for the label with 4 uuids.
+    Creates the ZPL code for the label with 3 uuids.
 ----------
     zpl : str
         The formatted ZPL code string that should be sent to the Zebra printer 
     """
     uuids = []
-    for i in range(4):
+    for i in range(3):
         uuids.append(str(uuid.uuid1()))
         # Ensure that we are not generating them to fast
         time.sleep(1. / 1e6)  # 1 us
 
+    # 3 UUIDS
     zpl = '''
 CT~~CD,~CC^~CT~
-^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4~SD24^JUS^LRN^CI0^XZ
+^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4~SD28^JUS^LRN^CI0^XZ
 ^XA
 ^MMT
-^PW650
-^LL0100
+^PW570
+^LL0150
 ^LS0
-^BY88,88^FT60,119^BXN,3,200,22,22,1,~
+^BY88,88^FT148,68^BXI,4,200,22,22,1,~
 ^FH\^FD{0}^FS
-^BY88,88^FT214,119^BXN,3,200,22,22,1,~
+^BY88,88^FT328,68^BXI,4,200,22,22,1,~
 ^FH\^FD{1}^FS
-^BY88,88^FT368,119^BXN,3,200,22,22,1,~
+^BY88,88^FT508,68^BXI,4,200,22,22,1,~
 ^FH\^FD{2}^FS
-^BY88,88^FT521,119^BXN,3,200,22,22,1,~
-^FH\^FD{3}^FS
-^PQ1,0,1,Y^XZ'''.format(uuids[0], uuids[1], uuids[2], uuids[3])
+^PQ1,0,1,Y^XZ'''.format(uuids[0],
+                        uuids[1],
+                        uuids[2])
+
+    # 4 UUIDS
+    # zpl = '''
+# CT~~CD,~CC^~CT~
+# ^XA~TA000~JSN^LT0^MNW^MTT^PON^PMN^LH0,0^JMA^PR4,4~SD24^JUS^LRN^CI0^XZ
+# ^XA
+# ^MMT
+# ^PW650
+# ^LL0100
+# ^LS0
+# ^BY88,88^FT60,119^BXN,3,200,22,22,1,~
+# ^FH\^FD{0}^FS
+# ^BY88,88^FT214,119^BXN,3,200,22,22,1,~
+# ^FH\^FD{1}^FS
+# ^BY88,88^FT368,119^BXN,3,200,22,22,1,~
+# ^FH\^FD{2}^FS
+# ^BY88,88^FT521,119^BXN,3,200,22,22,1,~
+# ^FH\^FD{3}^FS
+# ^PQ1,0,1,Y^XZ'''.format(uuids[0], uuids[1], uuids[2], uuids[3])
 
     del uuids
 
@@ -91,7 +111,7 @@ def print_labels(args):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((args.ip, PORT))
 
-    for ii in range(args.N // 4):
+    for ii in range(args.N // 3):
         zpl = create_label()
         if args.verbose > 0:
             print(zpl)
@@ -151,15 +171,15 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
     parser.add_argument('-V', '--version', action='version',
                         version=program_version_message)
     parser.add_argument('N', type=int, help='''Set the number of labels to be printed,
-if not a multiple of 4 will be rounded up to a multiple of ''')
+if not a multiple of 3 will be rounded up to a multiple of 3''')
     parser.add_argument('ip',  type=str,
                         help="Set the IP of the printer, format is '192.168.1.1'")
 
     # Process arguments
     args = parser.parse_args()
 
-    # Round up to nearest multiple of 4
-    args.N = args.N + (args.N % 4)
+    # Round up to nearest multiple of 3
+    args.N = args.N + (args.N % 3)
     if args.verbose > 0:
         print("Verbose mode on")
 
