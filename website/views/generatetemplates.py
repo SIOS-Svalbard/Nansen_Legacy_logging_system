0934c58a67eb620c0c115da9ebf52e33d811ae9f
lhmarsden <lhmarsden@hotmail.com>
2023-06-09 15:03:51 +0200

    generate template redirect reminder for later

--- website/views/generatetemplates.py
+++ website/views/generatetemplates.py
@@ -1,6 +1,4 @@
-from flask import Blueprint, render_template, request, send_file
-from website.lib.get_data import get_cruise
-from website import DB
+from flask import Blueprint, render_template, request, send_file, redirect
 
 generatetemplates = Blueprint('generatetemplates', __name__)
 
@@ -8,51 +6,8 @@ generatetemplates = Blueprint('generatetemplates', __name__)
 def generate_template():
     '''
     Generate template html page code
+    Should redirect to the page for the template generator (sub-tree) with LFNL config.
+    The redirect below doesn't work but serves as a reminder for later.
     '''
 
-    cruise_details_df = get_cruise(DB)
-    CRUISE_NUMBER = str(cruise_details_df['cruise_number'].item())
-
-    required_fields_dic, recommended_fields_dic, extra_fields_dic, groups = get_fields(configuration='activity', DB=DB, CRUISE_NUMBER=CRUISE_NUMBER)
-
-    added_fields_dic = {}
-    if request.method == 'POST':
-
-        form_input = request.form.to_dict(flat=False)
-
-        for key, val in form_input.items():
-            if key not in required_fields_dic.keys() and key not in recommended_fields_dic.keys() and key != 'submitbutton':
-                for field in fields.fields:
-                    if field['name'] == key:
-                        added_fields_dic[key] = {}
-                        added_fields_dic[key]['disp_name'] = field['disp_name']
-                        added_fields_dic[key]['description'] = field['description']
-
-        if form_input['submitbutton'] == ['generateTemplate']:
-
-            fields_list = list(required_fields_dic.keys())
-
-            for field, val in form_input.items():
-                if val == ['on']:
-                    fields_list = fields_list + [field]
-
-            filepath = '/tmp/generated_template.xlsx'
-
-            write_file(filepath, fields_list, metadata=True, conversions=True, data=False, metadata_df=False, DB=DB, CRUISE_NUMBER=CRUISE_NUMBER)
-
-            return send_file(filepath, as_attachment=True)
-
-    if len(added_fields_dic) > 0:
-        added_fields_bool = True
-    else:
-        added_fields_bool = False
-
-    return render_template(
-    "generateTemplate.html",
-    required_fields_dic = required_fields_dic,
-    recommended_fields_dic = recommended_fields_dic,
-    extra_fields_dic = extra_fields_dic,
-    groups = groups,
-    added_fields_dic = added_fields_dic,
-    added_fields_bool = added_fields_bool
-    )
+    return redirect("/config=Learnings from Nansen Legacy logging system")
