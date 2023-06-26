214a0386cb2ebd8d524b8c5be6aa6f5c2b16cb6e
lhmarsden <lhmarsden@hotmail.com>
2023-06-26 12:38:06 +0200

    Fixes #36

--- website/views/missingmetadata.py
+++ website/views/missingmetadata.py
@@ -1,138 +1,16 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from website.lib.get_data import get_personnel_df, get_cruise, get_metadata_for_list_of_ids, get_registered_niskins
+from website.lib.get_data import get_personnel_df, get_cruise, get_metadata_for_list_of_ids
 from website.lib.input_update_records import update_record_metadata_catalogue
-from website.lib.harvest_activities import harvest_activities
 from website.lib.checker import run as checker
 from website.lib.other_functions import split_personnel_list, combine_personnel_details, format_form_value
 from website import DB, TOKTLOGGER, FIELDS_FILEPATH, CONFIG
 from datetime import datetime as dt
 from math import isnan
-from website.lib.get_setup_for_configuration import get_setup_for_configuration
 from website.lib.get_dict_for_list_of_fields import get_dict_for_list_of_fields
 from website.lib.propegate_parents_to_children import propegate_parents_to_children
+from website.lib.get_missing import get_missing_activities, get_missing_niskins
 
 missingmetadata = Blueprint('missingmetadata', __name__)
-
-def get_missing_activities():
-    '''
-    Get a dataframe of missing activities
-    And a dictionary with the configuration for the required fields
-    '''
-    cruise_details_df = get_cruise(DB)
-    CRUISE_NUMBER = str(cruise_details_df['cruise_number'].item())
-
-    activities_df = harvest_activities(TOKTLOGGER, DB, CRUISE_NUMBER).reset_index()
-
-    # Loading fields
-    (
-        output_config_dict,
-        output_config_fields,
-        extra_fields_dict,
-        cf_standard_names,
-        dwc_terms,
-        dwc_terms_not_in_config,
-        all_fields_dict,
-        added_fields_dic,
-        added_cf_names_dic,
-        added_dwc_terms_dic,
-        groups
-    ) = get_setup_for_configuration(
-        fields_filepath=FIELDS_FILEPATH,
-        subconfig='Activities',
-        CRUISE_NUMBER=CRUISE_NUMBER
-    )
-
-    # Removing rows from dataframe where no missing values
-
-    # Checking rows logged without either depth NOR elevation (only one required)
-    # Minimum and maximum both required
-
-    # If min or max depth missing, depths_recorded = False
-    if 'minimumDepthInMeters' in output_config_dict['Data']['Recommended'].keys() and 'maximumDepthInMeters' in output_config_dict['Data']['Recommended'].keys():
-        activities_df['depths_recorded'] = ~activities_df[['minimumdepthinmeters','maximumdepthinmeters']].isna().any(axis = 1)
-    else:
-        activities_df['depths_recorded'] = True # So rows are not dropped later based on depth
-    # If min or max elevation missing, elevations_recorded = False
-    if 'minimumElevationInMeters' in output_config_dict['Data']['Recommended'].keys() and 'maximumElevationInMeters' in output_config_dict['Data']['Recommended'].keys():
-        activities_df['elevations_recorded'] = ~activities_df[['minimumelevationinmeters','maximumelevationinmeters']].isna().any(axis = 1)
-    else:
-        activities_df['elevations_recorded'] = True # So rows are not dropped later based on depth# If depths_recorded is True or elevations_recorded is True, then it okay.
-    # So keep if both are False
-
-    # Other fields
-    check_for_missing = list(output_config_dict['Data']['Required'].keys())
-    check_for_missing = [c.lower() for c in check_for_missing]
-    if 'pi_details' in check_for_missing:
-        check_for_missing.remove('pi_details')
-        check_for_missing = check_for_missing + ['pi_name', 'pi_email', 'pi_institution']
-    if 'recordedby' in check_for_missing:
-        check_for_missing.remove('recordedby')
-        check_for_missing = check_for_missing + ['recordedby_name', 'recordedby_email', 'recordedby_institution']
-
-    # If any required columns are missing, then 'all_required_present' = False
-    activities_df['all_required_present'] = ~activities_df[check_for_missing].isna().any(axis = 1)
-
-    # Keep all rows where all required_present is False AND
-    # EITHER depths recorded is False OR elevations recorded is False
-    activities_df = activities_df[(activities_df['all_required_present'] == False) | (activities_df['depths_recorded'] == False) & (activities_df['elevations_recorded'] == False)]
-
-    # Sorting dataframe
-    activities_df.sort_values(by=['eventdate', 'eventtime'], ascending=False, inplace=True)
-    activities_df = activities_df.reset_index()
-
-    return activities_df, output_config_dict
-
-def get_missing_niskins():
-    '''
-    Get a dataframe of missing activities
-    And a dictionary with the configuration for the required fields
-    '''
-    cruise_details_df = get_cruise(DB)
-    CRUISE_NUMBER = str(cruise_details_df['cruise_number'].item())
-
-    niskins_df = get_registered_niskins(DB, CRUISE_NUMBER).reset_index()
-
-    # Loading fields
-    (
-        output_config_dict,
-        output_config_fields,
-        extra_fields_dict,
-        cf_standard_names,
-        dwc_terms,
-        dwc_terms_not_in_config,
-        all_fields_dict,
-        added_fields_dic,
-        added_cf_names_dic,
-        added_dwc_terms_dic,
-        groups
-    ) = get_setup_for_configuration(
-        fields_filepath=FIELDS_FILEPATH,
-        subconfig='Niskin bottles',
-        CRUISE_NUMBER=CRUISE_NUMBER
-    )
-
-    # Removing rows from dataframe where no missing values
-    check_for_missing = list(output_config_dict['Data']['Required'].keys())
-    check_for_missing = [c.lower() for c in check_for_missing]
-    if 'pi_details' in check_for_missing:
-        check_for_missing.remove('pi_details')
-        check_for_missing = check_for_missing + ['pi_name', 'pi_email', 'pi_institution']
-    if 'recordedby' in check_for_missing:
-        check_for_missing.remove('recordedby')
-        check_for_missing = check_for_missing + ['recordedby_name', 'recordedby_email', 'recordedby_institution']
-
-    # If any required columns are missing, then 'all_required_present' = False
-    niskins_df['all_required_present'] = ~niskins_df[check_for_missing].isna().any(axis = 1)
-
-    # Keep all rows where all required_present is False AND
-    # EITHER depths recorded is False OR elevations recorded is False
-    niskins_df = niskins_df[(niskins_df['all_required_present'] == False)]
-
-    # Sorting dataframe
-    niskins_df.sort_values(by=['eventdate', 'eventtime', 'bottlenumber'], ascending=False, inplace=True)
-    niskins_df = niskins_df.reset_index()
-
-    return niskins_df, output_config_dict
 
 @missingmetadata.route('/missingMetadata', methods=['GET'])
 def missingmetadataredirect():
