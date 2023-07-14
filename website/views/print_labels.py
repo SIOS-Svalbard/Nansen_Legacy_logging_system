10c4980b00f78a02076816130cbc787f2f21e70a
lhmarsden <lhmarsden@hotmail.com>
2023-07-14 10:30:25 +0200

    values retrieved from form

--- website/views/print_labels.py
+++ website/views/print_labels.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, request
 from website import CONFIG
-
 
 print_labels = Blueprint('print_labels', __name__)
 
@@ -11,6 +10,10 @@ def print_medium_labels():
     ribbon = 'Zebra 5095, 84 mm'
     labels = 'CILS Eppendorf, 81000TN'
     ip = CONFIG['medium_label_printer']['ip']
+
+    increment3 = False
+    increment4 = False
+    number_labels = 1
 
     text = {
         1: { # Line 1
@@ -31,13 +34,27 @@ def print_medium_labels():
         }
     }
 
+    if request.method == "POST":
+
+        (
+            number_labels,
+            ip,
+            text,
+            increment3,
+            increment4,
+        ) = get_values_from_form(text)
+
+
     return render_template(
     "/print_labels.html",
     size=size,
     ribbon=ribbon,
     labels=labels,
     ip=ip,
-    text=text
+    text=text,
+    increment3=increment3,
+    increment4=increment4,
+    number_labels=number_labels
     )
 
 @print_labels.route('/print_large_labels', methods=['GET', 'POST'])
@@ -47,6 +64,10 @@ def print_large_labels():
     ribbon = 'BRADY R-6400, 65 mm'
     labels = 'BRADY 48.26 × 25.4 mm ,THT-156-492-1.5-SC'
     ip = CONFIG['large_label_printer']['ip']
+
+    increment3 = False
+    increment4 = False
+    number_labels = 1
 
     text = {
         1: { # Line 1
@@ -71,11 +92,44 @@ def print_large_labels():
         }
     }
 
+    if request.method == "POST":
+
+        (
+            number_labels,
+            ip,
+            text,
+            increment3,
+            increment4,
+        ) = get_values_from_form(text)
+
     return render_template(
     "/print_labels.html",
     size=size,
     ribbon=ribbon,
     labels=labels,
     ip=ip,
-    text=text
+    text=text,
+    increment3=increment3,
+    increment4=increment4,
+    number_labels=number_labels
     )
+
+def get_values_from_form(text):
+    '''
+    Retrieve values from form.
+    These values are preserved after the user hits print.
+    '''
+    form_input = request.form.to_dict(flat=False)
+    number_labels = form_input['number_labels'][0]
+    ip = form_input['ip'][0]
+    for line, criteria in text.items():
+        text[line]['content'] = form_input[f'line{line}'][0]
+    if 'increment3' in form_input and form_input['increment3'] == ['on']:
+        increment3 = True
+    else:
+        increment3 = False
+    if 'increment4' in form_input and form_input['increment4'] == ['on']:
+        increment4 = True
+    else:
+        increment4 = False
+    return number_labels, ip, text, increment3, increment4
